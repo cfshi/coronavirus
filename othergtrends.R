416b4762b408626a1111607bb0d641b2771f5466
wzmli <wzmichael.li@gmail.com>
2020-03-25 12:22:33 -0400

    updating the plot

--- othergtrends.R
+++ othergtrends.R
@@ -4,5 +4,5 @@ library(tidyverse)
 dec2today <- "2019-12-01 2020-03-25"
 lessthan1 <- 0.5
 
-flattrend <- gtrends("flatting the curve",time=dec2today,tz=0)
+flattrend <- gtrends("flattening the curve + flattening the curve",time=dec2today,tz=0)
 
