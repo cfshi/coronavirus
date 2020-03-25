8f50b0a198b3cf70bbdbb86002c5613cfa2a2fc8
wzmli <wzmichael.li@gmail.com>
2020-03-25 10:08:35 -0400

    ftc trends

--- combo_worldPlot.R
+++ combo_worldPlot.R
@@ -3,11 +3,12 @@ library(tidyverse)
 
 mindate <- as.Date("2020-01-15")
 
-gg <- (ggplot(combodat, aes(x=date,y=score,lty=type,color=term))
-#	+ geom_point()
+gg <- (ggplot(combodat, aes(x=date,y=score,lty=type,color=interaction(term,type)))
 	+ geom_line()
 	+ theme(legend.position="bottom")
 	+ xlim(mindate,NA)
+	+ scale_color_manual(values=c("red","blue","black","black"))
+	+ scale_linetype_manual(values=c(1,2))
 )
 
 
